2c6b0a5ebdade1cecf5b7220b63553cc8ff0bc49
shivamvishwakarm <vishwakarmashivam2003@gmail.com>
2026-08-16 10:16:48 +0530

    refactor(content): remove os section and rewrite blog section message

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -5,11 +5,15 @@ export const metadata = {
 
 export default function BlogPage() {
   return (
-    <div className="max-w-auto mx-auto px-4 md:max-w-3xl py-10">
+    <div className="max-w-auto mx-auto px-4 md:max-w-3xl py-10 ">
       <h2 className="text-xl font-semibold text-neutral-500 md:text-2xl">Blog</h2>
-      <p className="mt-6 text-neutral-400 dark:text-neutral-500 text-sm">
-        Coming soon...
-      </p>
+      <div className="flex space-y-2">
+
+        <h6 className="mt-6 text-neutral-400 dark:text-neutral-500 text-sm ">
+          You caught me 😮‍💨. I'm too lazy to write blogs.
+          <p className="text-amber-200/30 text-[0.8rem] text-right">I promise. My stories are coming...</p>
+        </h6>
+      </div>
     </div>
   );
 }
